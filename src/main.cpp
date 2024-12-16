3d65f88da51b9f812104e626efaeb73cb68eaafc
Anton Drewing <anton.drewing@gmail.com>
2024-12-16 15:14:22 +0100

    Working on graph parsing, currently broken.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 #include "nnbuilder.hpp"
 #include "onnx.pb.h"
 
+#include <windows.h>
+
 
 using namespace ninf;
 
@@ -19,11 +21,82 @@ onnx::GraphProto buildGraph() {
     onnx::ModelProto model;
     model.ParseFromIstream(&input); // parse file
     onnx::GraphProto graph = model.graph(); // the gragh
+
+    auto inputData = *graph.input().begin();
+    const auto &shape = inputData.type().tensor_type().shape();
+    int dim_size = shape.dim_size();
+    DimsData inputDims{1, 1, 1};
+    if (dim_size == 2) // First layer is FC layer.
+        inputDims[1] = shape.dim(1).dim_value();
+    else { // First layer is CONV layer (or exception is thrown).
+        inputDims[0] = shape.dim(1).dim_value();
+        inputDims[1] = shape.dim(2).dim_value();
+        inputDims[2] = shape.dim(3).dim_value();
+    }
+
+    NNBuilder nnBuilder{inputDims};
+
+    std::vector<std::string> opTypes{};
+    for (auto nodeData : graph.node()) {
+
+        for (auto nodeInputData : nodeData.input())
+            opTypes.push_back(nodeInputData);
+        // for (auto nodeInputData : nodeData.input()) {
+        //     std::cout << nodeInputData << std::endl;
+        // }
+        // auto nodeInputData = *nodeData.input().begin();
+        // std::cout << "OP_TYPE: " << nodeData.op_type() << std::endl;
+        // for (auto attributeData : nodeData.attribute()) {
+
+        // }
+
+        // auto nodeInputData = *nodeData.input().begin();
+        // const auto &shape = nodeInputData.begin();
+        // int dim_size = shape.dim_size();
+    }
+
+    for (int i = 1; i < dim_size; i++) { // First dim is batch_size.
+
+    }
+
+    // auto outp = graph.output();
+    // for (auto inputData : inp) {
+    //     const auto &shape = inputData.type().tensor_type().shape();
+    //     int dim_size = shape.dim_size();
+    //     for (int i = 1; i < dim_size; i++) {
+
+    //     }// First dim is batch_size.
+    //         std::cout << "Dim_" << i << " : " << shape.dim(i).dim_value() << std::endl;
+    //     std::cout << "Name: " << inputData.name() << std::endl;
+    //     for (auto dim : shape.dim()) {
+
+    //     }
+    // }
+    // for (auto inputData : outp) {
+    //     const auto &shape = inputData.type().tensor_type().shape();
+    //     int dim_size = shape.dim_size();
+    //     for (int i = 0; i < dim_size; i++)
+    //         std::cout << "Dim_" << i << " : " << shape.dim(i).dim_value() << std::endl;
+    //     std::cout << "Name: " << inputData.name() << std::endl;
+    //     for (auto dim : shape.dim()) {
+
+    //     }
+    // }
+    // for (auto node : graph.node()) {
+    //     for (auto attrib : node.attribute()) {
+    //         std::cout << "Name: " << attrib.name() << ",  Type: " << attrib.type() << std::endl;
+    //     }
+    //     // const auto &shape = valInfoData.type().tensor_type().shape();
+    //     // int dim_size = shape.dim_size();
+    //     // for (int i = 0; i < dim_size; i++)
+    //     //     std::cout << "Dim_" << i << " : " << shape.dim(i).dim_value() << std::endl;
+    //     std::cout << "---------------------------------" << std::endl;
+    // }
     return graph;
 }
 
 int main() {
-
+    SetConsoleOutputCP(CP_UTF8);
     NNBuilder nnBuilder{DimsData {3, 100, 100}};
     NeuralNetwork nn = nnBuilder
         .addConv2DLayer(10, 10, 10, relu)
@@ -36,6 +109,6 @@ int main() {
     auto outp = nn.get(inp);
     outp = nn.getVerbose(inp);
     auto activations = nn.getActivations();
-    auto graph = buildGraph();
+    buildGraph();
     return 0;
 }
